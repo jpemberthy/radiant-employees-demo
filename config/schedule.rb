002463b5cc7f9f3e9bb0351ede4a0aa6c7efc48a
jpemberthy <jpemberthy@gmail.com>
2009-11-09 21:09:37 -0500

    fixing task.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :cron_log, RAILS_ROOT + "/log/cron_log.log"
 
-every 1.hour do
-  command "git push heroku master"
-  command "heroku db:push"
+every 1.minute do
+  command "cd #{RAILS_ROOT} && git push heroku master"
+  command "cd #{RAILS_ROOT} && heroku db:push"
 end
 
