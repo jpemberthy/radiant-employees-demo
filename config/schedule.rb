1c4ef6731ed0a5d927cacf807f03bb3859eb6ab7
jpemberthy <jpemberthy@gmail.com>
2009-11-11 09:35:44 -0500

    restore database each hour.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,6 @@
 set :cron_log, RAILS_ROOT + "/log/cron_log.log"
 
 every 1.hour do
-  command "cd #{RAILS_ROOT} && git push heroku master"
-  command "cd #{RAILS_ROOT} && heroku db:push"
+  rake "db:dumpimport"
 end
 
