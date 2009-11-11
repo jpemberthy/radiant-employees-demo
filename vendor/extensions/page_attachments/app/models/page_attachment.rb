833455c7cc40f7cfc6d91df3a8585df3fe120360
jpemberthy <jpemberthy@gmail.com>
2009-11-10 22:18:46 -0500

    back to file_system.

--- vendor/extensions/page_attachments/app/models/page_attachment.rb
+++ vendor/extensions/page_attachments/app/models/page_attachment.rb
@@ -2,7 +2,7 @@ class PageAttachment < ActiveRecord::Base
   PAGE_ATTACHMENT_SIZES = { :icon => '50x50>', :thumb => '320x200>', :face => '75x75' }
   
   acts_as_list :scope => :page_id
-  has_attachment :storage => :s3, 
+  has_attachment :storage => :file_system, 
                  :thumbnails => defined?(PAGE_ATTACHMENT_SIZES) && PAGE_ATTACHMENT_SIZES || {:icon => '50x50>'},
                  :resize_to => '350x350',
                  :max_size => 10.megabytes,
